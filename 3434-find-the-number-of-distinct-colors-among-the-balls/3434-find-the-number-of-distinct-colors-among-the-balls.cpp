e565282fbec1544b3dd31e3464eb2d60a4e07916
Abhyudayaditya Kumar Singh <98250720+AbhyudayadityaKumarSingh@users.noreply.github.com>
2025-02-07 20:28:11 +0530

    Time: 1319 ms (5.15%), Space: 155.8 MB (66.56%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_map <int,int> btc ;
-        unordered_map <int,int> ctc ;
+        unordered_map<int, int> ball_to_color;  // Maps each ball to its current color
+        unordered_set<int> distinct_colors;  // Stores all distinct colors
+        vector<int> ans(queries.size());
 
-        
-        vector<int> ans ;
-         int cnt = 0 ;
-        for(int i=0;i<queries.size();i++) {
-            
-            int ball = queries[i][0] ;
-            int color = queries[i][1] ;
-            
-            if(btc.find(ball) != btc.end()) {
-                int old = btc[ball] ;
-                ctc[old] -- ;
-                if(ctc[old]==0) cnt -- ;
+        for (int i = 0; i < queries.size(); i++) {
+            int ball = queries[i][0];
+            int color = queries[i][1];
+
+            // If the ball already has a color, check if it was unique
+            if (ball_to_color.find(ball) != ball_to_color.end()) {
+                int old_color = ball_to_color[ball];
+
+                // Remove old color if it's no longer used
+                bool still_exists = false;
+                for (const auto& [key, val] : ball_to_color) {
+                    if (val == old_color && key != ball) {
+                        still_exists = true;
+                        break;
+                    }
+                }
+                if (!still_exists) {
+                    distinct_colors.erase(old_color);
+                }
             }
-           btc[ball]= color ;
-            ctc[color]++;
-            if(ctc[color]==1) cnt ++ ;
-            ans.push_back(cnt) ;
-            
+
+            // Assign new color to the ball
+            ball_to_color[ball] = color;
+            distinct_colors.insert(color);
+
+            // Store the result
+            ans[i] = distinct_colors.size();
         }
-        return ans ;
+
+        return ans;
     }
 };
