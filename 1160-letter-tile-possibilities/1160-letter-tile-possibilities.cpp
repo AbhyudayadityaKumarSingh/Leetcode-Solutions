41957c3c4cb3136c15cb72dccf8c90646cffe1d9
Abhyudayaditya Kumar Singh <98250720+AbhyudayadityaKumarSingh@users.noreply.github.com>
2025-02-18 13:16:46 +0530

    Time: 35 ms (41.85%), Space: 17 MB (46.49%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int n;
-
-    void solve(string& tiles, vector<bool>& used, unordered_set<string>& result, string &curr) {
-        result.insert(curr);
-
-        for(int i = 0; i < n; i++) {
-            if(used[i])
-                continue;
-
-            curr.push_back(tiles[i]);
-            used[i] = true;
-
-            solve(tiles, used, result, curr);
-
-            used[i] = false;
-            curr.pop_back();
+    int n ;
+    
+    void solve(string& tiles, string&temp ,unordered_set<string> &ans, vector<bool>& used ) {
+        ans.insert(temp) ;
+        for(int i=0;i<n;i++) {
+            if(used[i]) continue ;
+            temp.push_back(tiles[i]) ;
+            used[i] = true ;
+            solve(tiles,temp,ans,used) ;
+            used[i] = false ;
+            temp.pop_back() ;
         }
     }
-
     int numTilePossibilities(string tiles) {
-        n = tiles.length();
-        vector<bool> used(n, false);
-        unordered_set<string> result;
-        string curr = "";
-
-        solve(tiles, used, result, curr);
-
-        return result.size()-1;
+        n = tiles.size() ;
+        vector<bool> used(n,false) ;
+  
+        unordered_set<string> ans;
+        string temp = "" ;
+        solve(tiles,temp,ans,used) ;
+        return ans.size()-1 ;
     }
 };
